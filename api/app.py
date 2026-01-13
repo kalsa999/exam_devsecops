28d30797fb23e708610b6d89fcc7827c6116607c
khaled <k.salhi@mundiapolis.ma>
2026-01-13 09:55:30 +0100

    Correction Erreur

--- api/app.py
+++ api/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
 import hashlib
 import subprocess
+import os
 
 app = Flask(__name__)
 
 # Mot de passe en dur (mauvaise pratique)
-ADMIN_PASSWORD = "123456"
+#ADMIN_PASSWORD = "123456"
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "") 
+
 
 # Cryptographie faible (MD5)
 def hash_password(password):
+    #return hashlib.md5(password.encode()).hexdigest()
     return hashlib.md5(password.encode()).hexdigest()
 
 @app.route("/login")
@@ -17,8 +21,14 @@ def login():
     password = request.args.get("password")
 
     # Authentification faible
-    if username == "admin" and hash_password(password) == hash_password(ADMIN_PASSWORD):
-        return "Logged in"
+    #if username == "admin" and hash_password(password) == hash_password(ADMIN_PASSWORD):
+     #   return "Logged in"
+    if (
+        username == "admin"
+        and hash_password(password) == hash_password(ADMIN_PASSWORD)
+    ):
+        return {"status": "Logged in"}
+
 
     return "Invalid credentials"
 
@@ -28,9 +38,12 @@ def ping():
 
     # Injection de commande (shell=True)
     result = subprocess.check_output(
-        f"ping -c 1 {host}",
-        shell=True
+         ["ping", "-c", "1", host],
+        stderr=subprocess.STDOUT,
+        text=True
     )
+        
+    
     return result
 
 @app.route("/hello")
@@ -38,8 +51,11 @@ def hello():
     name = request.args.get("name", "user")
 
     # XSS potentiel
-    return f"<h1>Hello {name}</h1>"
+    #return f"<h1>Hello {name}</h1>"
+    return f"Hello {name}"
+
 
 if __name__ == "__main__":
     # Debug activé
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False)
